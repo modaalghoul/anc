915064689989f218f14d3fefc25a5cad17fda664
alghoul <modaalghoul@gmail.com>
2020-09-18 18:48:52 +0300

    Updates

---
+++ precache-manifest.cd42a3202041e22676b5473b1f8a72f2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51bdf2e43b7f67005e21b6240a2801e5",
+    "url": "/anc/index.html"
+  },
+  {
+    "revision": "5c549aedc425f3444275",
+    "url": "/anc/static/css/2.79c26e9f.chunk.css"
+  },
+  {
+    "revision": "77753d7ff0f2fb404b0f",
+    "url": "/anc/static/css/main.20ecfbf8.chunk.css"
+  },
+  {
+    "revision": "5c549aedc425f3444275",
+    "url": "/anc/static/js/2.8fe6b83d.chunk.js"
+  },
+  {
+    "revision": "43fafa47fcc31ad801a24b8595dc7aed",
+    "url": "/anc/static/js/2.8fe6b83d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77753d7ff0f2fb404b0f",
+    "url": "/anc/static/js/main.f34725b5.chunk.js"
+  },
+  {
+    "revision": "028cb99db8c7ea467b4d",
+    "url": "/anc/static/js/runtime-main.bd1df5f0.js"
+  },
+  {
+    "revision": "19582a26def207908121db9431dfe10e",
+    "url": "/anc/static/media/logo.19582a26.jpg"
+  }
+]);
